32b087b19315c88d0fe198fc37fa56be744938c8
Zakariakai <zakariakaifouf@gmail.com>
2024-11-08 03:31:04 +0100

    Initial commit with models and queries

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,28 +1,25 @@
-# relationship_app/views.py
+from .models import Book, Library
 from django.shortcuts import render
-from django.http import Http404
-from django.views import View
-from .models import Book, Library, Author
+from django.views.generic import DetailView
 
-# Function-based view for listing all books
+# Function-based view to list all books
 def list_books(request):
-    # Get all books from the database
+    # Retrieve all books from the database
     books = Book.objects.all()
-    
-    # Render the list_books.html template and pass the books context
+    # Render the books in the 'list_books.html' template
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
-# Class-based view for displaying details of a specific library
-class LibraryDetailView(View):
-    def get(self, request, library_name):
-        try:
-            # Get the library by name
-            library = Library.objects.get(name=library_name)
-        except Library.DoesNotExist:
-            raise Http404("Library not found")
-        
-        # Get all books in the library
-        books = library.books.all()
-        
-        # Render the library_detail.html template and pass the library and books context
-        return render(request, 'relationship_app/library_detail.html', {'library': library, 'books': books})
+# Class-based view to display a specific library's details
+class LibraryDetailView(DetailView):
+    # Specify the model for this view
+    model = Library
+    # The template to be used for rendering
+    template_name = 'relationship_app/library_detail.html'
+    # The name of the context variable that will contain the library data
+    context_object_name = 'library'
+
+    # Optionally, you can override the get_context_data method to add extra context
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Add custom context or filter the books here if needed
+        return context
